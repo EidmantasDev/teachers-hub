4261c6cf162226a83060126360d8838c522fb380
EidmantasDev <eidmantas.slenys@gmail.com>
2024-03-07 09:57:23 +0200

    Updated app module To use mongodb with .env file To main.ts added ValidationPipe for validation to work Updated Readme.md Downgraded mongodb and typeorm because it doesn't work with newest version Added .env-example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,35 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { LessonModule } from './lesson/lesson.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lesson.entity';
+import { StudentModule } from './student/student.module';
+import { Student } from './student/student.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env`],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mongodb',
+        url: configService.get('MONGO'),
+        synchronize: true,
+        useUnifiedTopology: true,
+        entities: [Lesson, Student],
+      }),
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    LessonModule,
+    StudentModule,
+  ],
 })
 export class AppModule {}
